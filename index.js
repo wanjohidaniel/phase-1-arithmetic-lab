8e3574be6e033ab1445588fe0e9ffde5cd3f017f
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-19 12:09:43 -0700

    changez

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const num2 = 62;
 let multiply = num1*num2;
 
 const random = Math.floor( Math.random() * 1000) + 1;
+
 let num3 = 43;
 let num4 = 13;
 const mod = num3 % num4;
+
 const max = Math.max( 5, 10, 15, 20 );
 
