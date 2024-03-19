01a6cea1db94fbc3ba17ca6c442ce98dd0d13661
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-19 11:54:14 -0700

    Function Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 1;
+const num2 = 62;
+let multiply = num1*num2;
 
-// Write your code here
+const random = Math.floor( Math.random() * 1000) + 1;
+
+let num3 = 43;
+let num4 = 13;
+const mod = num3 % num4;
+
+const max = Math.max( 5, 10, 15, 20 );
+
